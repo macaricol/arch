7b3a6245dbbb676527df8cfd134d70d2186788a8
Luis Maçarico <56197240+macaricol@users.noreply.github.com>
2025-10-18 11:46:07 +0100

    Update post_reboot.sh

--- post_reboot.sh
+++ post_reboot.sh
@@ -181,22 +181,22 @@ echo "###################### Setting keyboard layout #####################"
 echo "####################################################################"
 echo ""
 
-KDE_CONFIGS_DIR="$HOME/.config"
-[[ -d "$KDE_CONFIGS_DIR" ]] || mkdir -p "$KDE_CONFIGS_DIR"
+#KDE_CONFIGS_DIR="$HOME/.config"
+#[[ -d "$KDE_CONFIGS_DIR" ]] || mkdir -p "$KDE_CONFIGS_DIR"
 
 # Define the file path
-KEYB_FILE="$KDE_CONFIGS_DIR/kxkbrc"
+#KEYB_FILE="$KDE_CONFIGS_DIR/kxkbrc"
 
 # Create or overwrite the kxkbrc file with the specified content
-tee "$KEYB_FILE" > /dev/null << 'EOF'
-[Layout]
-LayoutList=pt
-Use=true
-EOF
+#tee "$KEYB_FILE" > /dev/null << 'EOF'
+#[Layout]
+#LayoutList=pt
+#Use=true
+#EOF
 
-echo "Created $KEYB_FILE with the specified content."
+#echo "Created $KEYB_FILE with the specified content."
 
-sleep 3
+#sleep 3
 
 clear
 echo "####################################################################"
@@ -252,6 +252,43 @@ if grep -q "file://$WALLPAPER_FILE" "$XML_FILE"; then
 else
   echo "Error: Failed to update $XML_FILE"
 fi
+
+echo "####################################################################"
+echo "#################### KDE Plasma configs init ####################"
+echo "####################################################################"
+echo ""
+
+# Set Portuguese (pt) as the only keyboard layout
+kwriteconfig6 --file kxkbrc --group Layout --key LayoutList "pt"
+kwriteconfig6 --file kxkbrc --group Layout --key Use "true"
+
+### Modern Clock
+git clone https://github.com/prayag2/kde_modernclock && cd kde_modernclock/
+
+kpackagetool6 -i package
+
+#kpackagetool6 --type Plasma/Applet -i
+
+#kpackagetool6 --type Plasma/Applet -l | grep modernclock
+
+# [Containments][1]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometries-1707x960 "Applet-25:896,256,432,160,0;"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometriesHorizontal "Applet-25:896,256,432,160,0;"
+
+# [Containments][1][Applets][25]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --key immutability "1"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --key plugin "com.github.prayag2.modernclock"
+
+# [Containments][1][Applets][25][Configuration][Appearance]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key date_font_color "205,227,251"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key day_font_color "242,116,223"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key day_font_size "40"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key time_font_color "205,227,251"
+
+# [Containments][1][Applets][25][Configuration][ConfigDialog]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group ConfigDialog --key DialogHeight "540"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group ConfigDialog --key DialogWidth "720"
+
 
 echo "####################################################################"
 echo "################ Enabling and starting sddm service ################"
