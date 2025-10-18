6c82ace621385851d9ac93b0c0407a09d7101ed6
Luis Maçarico <56197240+macaricol@users.noreply.github.com>
2025-10-18 17:23:17 +0100

    Update kde_init.sh

--- kde_init.sh
+++ kde_init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#requires delay otherwise kde first boot will overwrite what is being applied here
+sleep 10
+
 #Apply Dark Theme
 plasma-apply-colorscheme BreezeDark
 plasma-apply-desktoptheme breeze-dark
