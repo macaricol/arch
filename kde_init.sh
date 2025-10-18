adea12543c68669ebd2f1b609cb8c985ca7c706d
ishmael <legendary.ishmael@proton.me>
2025-10-18 15:30:57 +0100

    ...

---
+++ kde_init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+plasma-apply-desktoptheme breeze-dark
+
+# [Containments][1]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometries-1707x960 "Applet-100:896,256,432,160,0;"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometriesHorizontal "Applet-100:896,256,432,160,0;"
+
+# [Containments][1][Applets][100]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --key immutability "1"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --key plugin "com.github.prayag2.modernclock"
+
+# [Containments][1][Applets][100][Configuration][Appearance]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group Appearance --key date_font_color "205,227,251"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group Appearance --key day_font_color "242,116,223"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group Appearance --key day_font_size "40"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group Appearance --key time_font_color "205,227,251"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group Appearance --key use_24_hour_format "true"
+
+# [Containments][1][Applets][100][Configuration][ConfigDialog]
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group ConfigDialog --key DialogHeight "540"
+kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 100 --group Configuration --group ConfigDialog --key DialogWidth "720"
--- post.sh
+++ post.sh
@@ -199,8 +199,6 @@ echo ""
 kwriteconfig6 --file kxkbrc --group Layout --key LayoutList "pt"
 kwriteconfig6 --file kxkbrc --group Layout --key Use "true"
 
-#plasma-apply-desktoptheme breeze-dark
-
 # Desktop effect: Overview on right edge
 kwriteconfig6 --file kwinrc --group Effect-overview --key BorderActivate "2"
 # ElectricBorders: Show Desktop on bottom-right edge
@@ -213,23 +211,24 @@ git clone https://github.com/prayag2/kde_modernclock && cd kde_modernclock/
 
 kpackagetool6 -i package
 
-# [Containments][1]
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometries-1707x960 "Applet-25:896,256,432,160,0;"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --key ItemGeometriesHorizontal "Applet-25:896,256,432,160,0;"
+#copy plasma session autostartscript
+script_path="$HOME/.config/plasma-workspace/env"
+url="https://raw.githubusercontent.com/macaricol/arch/refs/heads/main/kde_init.sh"
 
-# [Containments][1][Applets][25]
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --key immutability "1"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --key plugin "com.github.prayag2.modernclock"
+# Ensure the directory exists
+mkdir -p "$script_path"
 
-# [Containments][1][Applets][25][Configuration][Appearance]
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key date_font_color "205,227,251"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key day_font_color "242,116,223"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key day_font_size "40"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group Appearance --key time_font_color "205,227,251"
+# Download the script
+if ! curl -s -o "$script_path/kde_init.sh" "$url"; then
+    echo "Error: Failed to download script"
+    exit 1
+fi
 
-# [Containments][1][Applets][25][Configuration][ConfigDialog]
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group ConfigDialog --key DialogHeight "540"
-kwriteconfig6 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Applets --group 25 --group Configuration --group ConfigDialog --key DialogWidth "720"
+# Make the script executable
+if ! chmod +x "$script_path/kde_init.sh"; then
+    echo "Error: Failed to set executable permissions"
+    exit 1
+fi
 
 
 echo "####################################################################"
